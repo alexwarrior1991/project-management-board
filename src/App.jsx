b6208b6e05bc2c6f4ff621501a2a80b779f222d2
alejandro <aguerreroegido@gmail.com>
2026-08-24 13:18:26 +0100

    add NewProject and Input components; update App layout to include NewProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
     return (
-        <main className="h-screen my-8">
+        <main className="h-screen my-8 flex gap-8">
             <ProjectsSideBar/>
+            <NewProject/>
         </main>
     );
 }
