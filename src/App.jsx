97bf87a2423bd531a73dad022a038fc37354f9c2
alejandro <aguerreroegido@gmail.com>
2026-09-24 12:00:58 +0100

    add Modal component; integrate into NewProject for input validation and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [projectState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    const handleStartAddProject = () => {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        })
+    };
+
+    const handleCancelAddProject = () => {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+            }
+        })
+    };
+
+    const handleAddProject = (projectData) => {
+        setProjectState(prevState => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId
+            };
+
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: [...prevState.projects, newProject]
+            };
+        })
+    };
+
+
+    let content;
+
+    if (projectState.selectedProjectId === null) {
+        content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+    } else if (projectState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSideBar/>
-            <NoProjectSelected/>
+            <ProjectsSideBar
+                onStartAddProject={handleStartAddProject}
+                projects={projectState.projects}
+            />
+            {content}
         </main>
     );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
 
     return (
         <div className="mt-4 text-center w-2/3">
@@ -17,7 +17,7 @@ export default function NoProjectSelected() {
                 Select project or get started with a new one
             </p>
             <p>
-                <Button>
+                <Button onClick={onStartAddProject}>
                     Create new project
                 </Button>
             </p>
